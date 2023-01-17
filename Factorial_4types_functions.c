43a002b5aee296168704ae1e46e214c6e7aecfba
Anshika Aggarwal <121662708+ANSHIKAGGARWAL@users.noreply.github.com>
2023-01-17 14:51:15 +0530

    Update Factorial_4types_functions.c

--- Factorial_4types_functions.c
+++ Factorial_4types_functions.c
@@ -16,3 +16,22 @@ scanf("%d",&number);
 factorial(number);
 }
 
+//WITH ARGUMENT AND WITH RETURN TYPE
+#include<stdio.h>
+int factorial(int number){   //DECLARING FUNCTION
+int output=1;
+while(number!=0){            //DEFINING FUNCTION
+output*=number;
+number--;}
+//printf("\t Factorial is %d",output);
+return output;
+}
+int main()
+{
+int number,calling_factorial;//DECLARING LOCAL VARIABLE
+printf("\n\t Enter the number to find the factorial\n");
+scanf("%d",&number);
+calling_factorial=factorial(number);//CALLING FUNCTION
+printf("\t FACTORIAL IS %d",calling_factorial);
+return 0;
+}
